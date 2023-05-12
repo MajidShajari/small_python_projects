6222527e06e891bd1f6f352d789247cfdf8721de
Majid Shajari <majidpy89@gmail.com>
2023-05-12 18:29:44 +0330

    Create caesar cipher Project

---
+++ caesarcipher/README.md
@@ -0,0 +1,25 @@
+# Caesar Cipher
+
+The Caesar Cipher is a simple encryption technique that shifts letters by a certain key value. This Python script allows you to encrypt or decrypt messages using the Caesar Cipher.
+
+## Requirements
+
+    Python 3.x
+    pyperclip module for copy translate message to clipboard. (installable via `pip install Bext`)
+
+## Customization
+
+You can customize the behavior of the program by modifying the following constants in the code:
+
+-   `SYMBOLS`: Controls the main alphabet for check and encrypt message
+
+## Usage
+
+    Clone or download the repository.
+    Install the `bext` module by running `pip install pyperclip`.
+    Navigate the project directory .
+    Run `python caesar_cipher.py`.
+
+## Contributing
+
+Contributions are welcome. Please feel free to open an issue or submit a pull request.
---
+++ caesarcipher/caesar_cipher.py
@@ -0,0 +1,66 @@
+"""Caesar Cipher, by Al Sweigart al@inventwithpython.com
+The Caesar cipher is a shift cipher that uses addition and subtraction
+to encrypt and decrypt letters.
+Tags: short, beginner, cryptography, math"""
+import sys
+
+
+def main():
+    print("Caesar Cipher")
+    print("The Caesar cipher encrypts letters by shifting them over by a")
+    print("key number. For example, a key of 2 means the letter A is")
+    print("encrypted into C, the letter B encrypted into D, and so on.")
+    print()
+    print('Do you want to (e)ncrypt or (d)ecrypt?')
+    _mode = input('> ').lower()
+
+    while _mode not in ['e', 'd']:
+        print('Please enter the letter e or d.')
+        _mode = input('> ').lower()
+    if _mode.startswith('e'):
+        _mode = "encrypt"
+    else:
+        _mode = "decrypt"
+
+    while True:  # Keep asking until the user enters a valid key.
+        max_key = len(SYMBOLS) - 1
+        print(f"Please enter the key (0 to {max_key}) to use.")
+        response = input('> ').upper()
+        if not response.isdecimal():
+            continue
+
+        if 0 <= int(response) < len(SYMBOLS):
+            key = int(response)
+            break
+
+    print(f"Enter the message to {_mode}.")
+    message = input('> ').upper()
+
+    _translate = ''
+
+    for symbol in message:
+        if symbol in SYMBOLS:
+            num = SYMBOLS.find(symbol)
+            if _mode == 'encrypt':
+                num = (num + key) % len(SYMBOLS)
+            elif _mode == 'decrypt':
+                num = (num - key) % len(SYMBOLS)
+            _translate += SYMBOLS[num]
+        else:
+            _translate += symbol
+
+    print(f"The Translate message : {_translate}")
+    return _translate, _mode
+
+
+try:
+    SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    translate, mode = main()
+    import pyperclip
+    pyperclip.copy(translate)
+    print(f"Full {mode}ed text copied to clipboard.")
+except ImportError:
+    print('This program requires the pyperclip module, for copy translate message to clipboard.')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
